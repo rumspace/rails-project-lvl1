7f711a4ada70e387a8e39c1ffb5a0eaeb2f14cef
rumspace <rumspace@gmail.com>
2022-06-14 18:30:14 +0400

    Adding cols and rows options in TextArea

--- lib/hexlet_code/form_elements/form_textarea.rb
+++ lib/hexlet_code/form_elements/form_textarea.rb
@@ -4,9 +4,9 @@ require_relative "../tag"
 
 # Form <textarea> tag generator
 class FormTextarea
-  def self.build(field, value)
-    cols = 20
-    rows = 40
+  def self.build(field, value, cols = 20, rows = 40)
+    # cols = 20
+    # rows = 40
     "#{Tag.build("textarea", cols: cols, rows: rows, name: field)}#{value}#{Tag.build("/textarea")}"
   end
 end
